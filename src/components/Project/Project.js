c48722aae01fa7698a6ad9a3a672e8bb50f01994
zisilva <zimarlensilva@gmail.com>
2022-11-28 09:26:51 -0800

    dance school

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,7 +1,7 @@
 import image2 from "../../images/whoIsTheVillain.png";
 import image3 from "../../images/TSW.png";
 import image4 from "../../images/Tranquili-Tea.png";
-import image5 from "../../images/Screenshot 2022-11-28 at 9.14.40 AM.png";
+import image5 from "../../images/danceschool.jpeg";
 import { FaDownload } from "react-icons/fa";
 
 export function Project() {
